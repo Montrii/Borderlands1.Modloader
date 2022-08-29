93b0d5344e5ee7a9786cf7a0e9189a5236017f45
Justin <jg68332@gmail.com>
2022-08-29 20:42:02 +0200

    Test to see if modloader gets loaded by injector
    Works.

---
+++ borderlands1modloader/dllmain.cpp
@@ -0,0 +1,14 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+    {
+        MessageBox(NULL, L"Loaded successfully", L"Success", MB_OK);
+    }
+    return TRUE;
+}
